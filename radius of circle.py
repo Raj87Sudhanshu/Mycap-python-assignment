09900e118e37a122b86036ef1055f9328a27cb0d
Raj87Sudhanshu <103066186+Raj87Sudhanshu@users.noreply.github.com>
2022-04-24 15:46:43 +0530

    Delete radius of circle.py

--- radius of circle.py
+++
@@ -1,7 +0,0 @@
-#input
-circle = float(input("enter the radius of the circle:"))
-print("the area of the circle:", 22/7*circle**2)
-
-#output
-enter the radius of the circle:1.1
-the area of the circle: 3.8028571428571434
