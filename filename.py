8685a3fb50307dd04ac3e3fd178739cadf179d5f
Raj87Sudhanshu <103066186+Raj87Sudhanshu@users.noreply.github.com>
2022-04-24 16:02:12 +0530

    Delete filename.py

--- filename.py
+++
@@ -1,8 +0,0 @@
-#input
-filename = input("Input the Filename: ")
-f_extns = filename.split(".")
-print ("The extension of the file is : " + repr(f_extns[-1]))
-
-#output
-Input the Filename: abc.python
-The extension of the file is : 'python'
