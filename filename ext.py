48de0ad274e02d542740196f962edea99931d896
Raj87Sudhanshu <103066186+Raj87Sudhanshu@users.noreply.github.com>
2022-04-24 16:37:43 +0530

    Update filename ext.py

--- filename ext.py
+++ filename ext.py
@@ -1,3 +1,6 @@
 filename = input("Input the Filename: ")
 f_extns = filename.split(".")
 print ("The extension of the file is : " + repr(f_extns[-1]))
+
+#input = Input the Filename: abc.py
+#output = The extension of the file is : 'py'
