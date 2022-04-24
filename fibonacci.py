7e566bce19c531827c6dddd235d2ce86e7480d88
Raj87Sudhanshu <103066186+Raj87Sudhanshu@users.noreply.github.com>
2022-04-24 16:27:49 +0530

    fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+nterms = int(input ("How many terms you wants to print: "))  
+ 
+n1 = 0  
+n2 = 1  
+count = 0  
+  
+if nterms <= 0:  
+    print ("Please enter a positive integer, the given number is not valid")  
+elif nterms == 1:  
+    print ("The Fibonacci sequence of the numbers up to", nterms, ": ")  
+    print(n1)  
+else:  
+    print ("The fibonacci sequence of the numbers is:")  
+    while count < nterms:  
+        print(n1)  
+        nth = n1 + n2  
+        n1 = n2  
+        n2 = nth  
+        count += 1 
