2591b953c8690ee8494f042bcef5bb2781a62cd4
Raj87Sudhanshu <103066186+Raj87Sudhanshu@users.noreply.github.com>
2022-04-24 16:34:34 +0530

    Update circle radius.py

--- circle radius.py
+++ circle radius.py
@@ -1,2 +1,5 @@
 circle = float(input("enter the radius of the circle:"))
 print("the area of the circle:", 22/7*circle**2)
+
+#enter the radius of the circle:1.1
+#the area of the circle: 3.8028571428571434
